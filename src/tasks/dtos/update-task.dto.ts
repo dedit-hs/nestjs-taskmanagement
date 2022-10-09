a875972979e68d80695d192678aaef8918ce4125
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2022-10-09 11:07:22 +0700

    Update validation

--- src/tasks/dtos/update-task.dto.ts
+++ src/tasks/dtos/update-task.dto.ts
@@ -1,11 +1,13 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../Models/task-status.enum';
 
 export class UpdateTaskDto {
   @IsOptional()
+  @IsNotEmpty()
   title: string;
 
   @IsOptional()
+  @IsNotEmpty()
   description: string;
 
   @IsOptional()
